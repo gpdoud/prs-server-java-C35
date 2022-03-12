8a26d31ab170839af351924670074e63d2466529
Greg Doud <gpdoud@gmail.com>
2022-03-12 16:10:25 -0500

    fixed request recalculation

--- src/main/java/com/maxtrain/capstone/requestline/RequestlineController.java
+++ src/main/java/com/maxtrain/capstone/requestline/RequestlineController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 
+import com.maxtrain.capstone.request.RequestRepository;
+
 
 @CrossOrigin
 @RestController
@@ -12,24 +14,24 @@ public class RequestlineController {
 
 	@Autowired
 	private RequestlineRepository reqlRepo;
-//	@Autowired
-//	private RequestRepository reqRepo;
+	@Autowired
+	private RequestRepository reqRepo;
 	
-//	@SuppressWarnings("rawtypes")
-//	private ResponseEntity recalcRequestTotal(int requestId) {
-//		var req = reqRepo.findById(requestId);
-//		if(req.isEmpty()) {
-//			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-//		}
-//		var requestTotal = 0;
-//		var reqlines = reqlRepo.findByRequest(req.get());
-//		for(var invl : reqlines) {
-//			requestTotal += invl.getProduct().getPrice() * invl.getQuantity();
-//		}
-//		inv.get().setTotal(requestTotal);
-//		reqRepo.save(req.get());
-//		return new ResponseEntity<>(HttpStatus.OK);
-//	}
+	@SuppressWarnings("rawtypes")
+	private ResponseEntity recalcRequestTotal(int requestId) {
+		var reqOpt = reqRepo.findById(requestId);
+		if(reqOpt.isEmpty()) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		var request = reqOpt.get();
+		var requestTotal = 0;
+		for(var requestline : request.getRequestlines()) {
+			requestTotal += requestline.getProduct().getPrice() * requestline.getQuantity();
+		}
+		request.setTotal(requestTotal);
+		reqRepo.save(request);
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
 
 	@GetMapping
 	public ResponseEntity<Iterable<Requestline>> getRequestlines() {
@@ -47,36 +49,50 @@ public class RequestlineController {
 	}
 	
 	@PostMapping
-	public ResponseEntity<Requestline> postRequestline(@RequestBody Requestline request) {
-		if(request == null || request.getId() != 0) {
+	public ResponseEntity<Requestline> postRequestline(@RequestBody Requestline requestline) throws Exception {
+		if(requestline == null || requestline.getId() != 0) {
 			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 		}
-		var prod = reqlRepo.save(request);
-		return new ResponseEntity<Requestline>(prod, HttpStatus.CREATED);
+		var reql = reqlRepo.save(requestline);
+		var respEntity = this.recalcRequestTotal(reql.getRequest().getId());
+		if(respEntity.getStatusCode() != HttpStatus.OK) {
+			throw new Exception("Recalculate request total failed!");
+		}
+		return new ResponseEntity<Requestline>(reql, HttpStatus.CREATED);
 	}
 	
 	@SuppressWarnings("rawtypes")
 	@PutMapping("{id}")
-	public ResponseEntity putRequestline(@PathVariable int id, @RequestBody Requestline request) {
-		if(request == null || request.getId() == 0) {
+	public ResponseEntity putRequestline(@PathVariable int id, @RequestBody Requestline requestline) throws Exception {
+		if(requestline == null || requestline.getId() == 0) {
 			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 		}
-		var prod = reqlRepo.findById(request.getId());
-		if(prod.isEmpty()) {
+		var reqlOpt = reqlRepo.findById(requestline.getId());
+		if(reqlOpt.isEmpty()) {
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 		}
-		reqlRepo.save(request);
+		var reql = reqlOpt.get();
+		reqlRepo.save(reql);
+		var respEntity = this.recalcRequestTotal(reql.getRequest().getId());
+		if(respEntity.getStatusCode() != HttpStatus.OK) {
+			throw new Exception("Recalculate request total failed!");
+		}
 		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 	}
 	
 	@SuppressWarnings("rawtypes")
 	@DeleteMapping("{id}")
-	public ResponseEntity deleteRequestline(@PathVariable int id) {
-		var prod = reqlRepo.findById(id);
-		if(prod.isEmpty()) {
+	public ResponseEntity deleteRequestline(@PathVariable int id) throws Exception {
+		var requestOpt = reqlRepo.findById(id);
+		if(requestOpt.isEmpty()) {
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 		}
-		reqlRepo.delete(prod.get());
+		var request = requestOpt.get();
+		reqlRepo.delete(request);
+		var respEntity = this.recalcRequestTotal(request.getId());
+		if(respEntity.getStatusCode() != HttpStatus.OK) {
+			throw new Exception("Recalculate request total failed!");
+		}
 		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 	}
 
--- src/main/java/com/maxtrain/capstone/requestline/RequestlineRepository.java
+++ src/main/java/com/maxtrain/capstone/requestline/RequestlineRepository.java
@@ -3,5 +3,5 @@ package com.maxtrain.capstone.requestline;
 import org.springframework.data.repository.CrudRepository;
 
 public interface RequestlineRepository extends CrudRepository<Requestline, Integer> {
-
+	Iterable<Requestline> findByRequestId(int requestId);
 }
